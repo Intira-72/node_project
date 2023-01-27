fa86d86031db7647def260880a09a0950591c617
xuadmin <xuadmin@Xubuntu.xubuntu.virtualbox.org>
2023-01-27 13:44:46 +0700

    Add One Page Website Temp

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import Debug from 'debug';
 import chalk from 'chalk';
 import morgan from 'morgan';
+import path from 'path';
+
 
 const app = express()
 
@@ -9,6 +11,7 @@ const debug = Debug('app')
 const port = 3000;
 
 app.use(morgan('combined'))
+app.use(express.static(path.dirname('./public/index.html')))
 
 app.get("/", (req, res) => {
     res.send("Hello BorntoDev");
