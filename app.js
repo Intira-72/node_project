25f7d6747e360297a1b461d33b051f073f8361bc
xuadmin <xuadmin@Xubuntu.xubuntu.virtualbox.org>
2023-02-02 10:22:57 +0700

    express router

--- app.js
+++ app.js
@@ -9,12 +9,23 @@ const app = express()
 
 const debug = Debug('app')
 const PORT = process.env.PORT;
+const productRouter = express.Router()
 
 app.use(morgan('combined'))
 app.use(express.static(path.dirname('./public/index.html')))
 
 app.set("views", "./src/views")
 app.set("view engine", "ejs")
+
+productRouter.route("/").get((req, res) => {
+    res.send("Hello, I'm Products.")
+})
+
+productRouter.route("/1").get((req, res) => {
+    res.send("Hello, I'm Products 1.")
+})
+
+app.use("/products", productRouter)
 
 app.get("/", (req, res) => {
     res.render('index', {username: 'BorntoDev', customer: ['Kitti', 'Kittikorn', 'Kitty']});
